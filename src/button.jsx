e9c1dcb117a52202c204bc14c15f097ff5406a92
Blast101 <119773274+Blast101@users.noreply.github.com>
2023-12-23 13:03:18 +0530

    Updated 23/12/2023

---
+++ src/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function button() {
+  return (
+    <>
+    <button style={{width:"100px",height:"100px",backgroundColor:"brown"}}>Click</button>
+    </>
+  )
+}
+
+export default button
