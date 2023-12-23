606b030bde04678ca3cbd9946d8cd0233769bbc7
Blast101 <119773274+Blast101@users.noreply.github.com>
2023-12-23 13:30:58 +0530

    New Branch
    Branch 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './button'
+import Box from './Box'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
   <Button/>      
+  <Box/>
     </>
   )
 }
---
+++ src/Box.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Box() {
+  return (
+    <div style={{width:"100px",height:"100px",backgroundColor:"brown"}}>Box</div>
+  )
+}
+
+export default Box
